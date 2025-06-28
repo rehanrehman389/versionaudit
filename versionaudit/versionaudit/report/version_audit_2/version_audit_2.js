506c210d67e0cce723f6d55930d156de59fa7baa
Rehan Ansari <32939507+rehanrehman389@users.noreply.github.com>
2025-06-28 17:24:36 +0530

    Update version_audit_2.js

--- versionaudit/versionaudit/report/version_audit_2/version_audit_2.js
+++ versionaudit/versionaudit/report/version_audit_2/version_audit_2.js
@@ -10,8 +10,9 @@ function get_filters() {
             options: "DocType",
             reqd: 1,
             onchange: function () {
-                // Clear the docname filter when doctype changes
                 frappe.query_report.set_filter_value("docname", null);
+                frappe.query_report.set_filter_value("child_table", null);
+                frappe.query_report.set_filter_value("view_type", "Document Level");
             },
         },
         {
@@ -20,16 +21,54 @@ function get_filters() {
             fieldtype: "MultiSelectList",
             get_data: function (txt) {
                 const selected_doctype = frappe.query_report.get_filter_value("doctype");
-                if (!selected_doctype) {
-                    return [];
-                }
-                // Fetch document names dynamically based on the selected doctype
+                if (!selected_doctype) return [];
                 return frappe.db.get_link_options(selected_doctype, txt);
             },
         },
+        {
+            fieldname: "view_type",
+            label: __("View Type"),
+            fieldtype: "Select",
+            options: ["Document Level", "Child Table Diff"],
+            default: "Document Level",
+            onchange: function () {
+                const viewType = frappe.query_report.get_filter_value("view_type");
+                const doctype = frappe.query_report.get_filter_value("doctype");
+
+                if (viewType === "Child Table Diff" && doctype) {
+                    frappe.call({
+                        method: "frappe.desk.query_report.get_script", // dummy to trigger doctype meta load
+                        args: { report_name: "Version Audit 2" },
+                        callback: function () {
+                            const meta = frappe.get_meta(doctype);
+                            const child_tables = meta.fields
+                                .filter(df => df.fieldtype === "Table")
+                                .map(df => ({ label: df.label, value: df.fieldname }));
+                            frappe.query_report.get_filter("child_table").df.options = child_tables;
+                            frappe.query_report.set_filter_value("child_table", null);
+                            frappe.query_report.refresh_filter("child_table");
+                        }
+                    });
+                }
+            }
+        },
+        {
+            fieldname: "child_table",
+            label: __("Child Table"),
+            fieldtype: "Select",
+            hidden: 1,
+        }
     ];
 }
 
 frappe.query_reports["Version Audit 2"] = {
     filters: get_filters(),
+    onload: function (report) {
+        frappe.query_report.get_filter("view_type").onchange();
+    },
+    get_datatable_options(options) {
+        return Object.assign({}, options, {
+            dynamicRowHeight: true,
+        });
+    }
 };
