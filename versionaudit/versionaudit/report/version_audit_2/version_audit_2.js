e2dd9405acbc3e89019ca7697100aba451d978f0
Rehan Ansari <32939507+rehanrehman389@users.noreply.github.com>
2025-06-28 18:40:49 +0530

    Update version_audit_2.js

--- versionaudit/versionaudit/report/version_audit_2/version_audit_2.js
+++ versionaudit/versionaudit/report/version_audit_2/version_audit_2.js
@@ -12,7 +12,6 @@ function get_filters() {
             onchange: function () {
                 frappe.query_report.set_filter_value("docname", null);
                 frappe.query_report.set_filter_value("child_table", null);
-                frappe.query_report.set_filter_value("view_type", "Document Level");
             },
         },
         {
@@ -21,7 +20,9 @@ function get_filters() {
             fieldtype: "MultiSelectList",
             get_data: function (txt) {
                 const selected_doctype = frappe.query_report.get_filter_value("doctype");
-                if (!selected_doctype) return [];
+                if (!selected_doctype) {
+                    return [];
+                }
                 return frappe.db.get_link_options(selected_doctype, txt);
             },
         },
@@ -29,46 +30,38 @@ function get_filters() {
             fieldname: "view_type",
             label: __("View Type"),
             fieldtype: "Select",
-            options: ["Document Level", "Child Table Diff"],
-            default: "Document Level",
+            options: ["Document Level Diff", "Child Table Diff"],
+            default: "Document Level Diff",
             onchange: function () {
-                const viewType = frappe.query_report.get_filter_value("view_type");
-                const doctype = frappe.query_report.get_filter_value("doctype");
-
-                if (viewType === "Child Table Diff" && doctype) {
-                    frappe.call({
-                        method: "frappe.desk.query_report.get_script", // dummy to trigger doctype meta load
-                        args: { report_name: "Version Audit 2" },
-                        callback: function () {
-                            const meta = frappe.get_meta(doctype);
-                            const child_tables = meta.fields
-                                .filter(df => df.fieldtype === "Table")
-                                .map(df => ({ label: df.label, value: df.fieldname }));
-                            frappe.query_report.get_filter("child_table").df.options = child_tables;
-                            frappe.query_report.set_filter_value("child_table", null);
-                            frappe.query_report.refresh_filter("child_table");
-                        }
-                    });
-                }
-            }
+                frappe.query_report.refresh();  // To toggle child_table filter visibility
+            },
         },
         {
             fieldname: "child_table",
             label: __("Child Table"),
             fieldtype: "Select",
-            hidden: 1,
+            depends_on: "eval:doc.view_type === 'Child Table Diff'",
+            get_data: function () {
+                const doctype = frappe.query_report.get_filter_value("doctype");
+                if (!doctype) return [];
+
+                return frappe.meta.get_docfield(doctype, null).then(meta_fields => {
+                    return meta_fields
+                        .filter(df => df.fieldtype === "Table")
+                        .map(df => ({ label: df.label, value: df.fieldname }));
+                });
+            },
+        },
+        {
+            fieldname: "include_empty_fields",
+            label: __("Include Empty Fields"),
+            fieldtype: "Check",
+            default: 0,
+            depends_on: "eval:doc.view_type === 'Document Level Diff'"
         }
     ];
 }
 
 frappe.query_reports["Version Audit 2"] = {
-    filters: get_filters(),
-    onload: function (report) {
-        frappe.query_report.get_filter("view_type").onchange();
-    },
-    get_datatable_options(options) {
-        return Object.assign({}, options, {
-            dynamicRowHeight: true,
-        });
-    }
+    filters: get_filters()
 };
