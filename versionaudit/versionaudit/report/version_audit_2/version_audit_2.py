3d1ab334546719221b0a6ec6bd2e0c3a62726ea4
Rehan Ansari <32939507+rehanrehman389@users.noreply.github.com>
2025-06-28 17:39:31 +0530

    Update version_audit_2.py

--- versionaudit/versionaudit/report/version_audit_2/version_audit_2.py
+++ versionaudit/versionaudit/report/version_audit_2/version_audit_2.py
@@ -8,16 +8,7 @@ from frappe.utils import get_datetime
 def execute(filters=None):
     doctype = filters.get("doctype")
     docname_filter = filters.get("docname")
-    view_type = filters.get("view_type") or "Document Level"
-    child_table_field = filters.get("child_table")
-    columns, data = [], []
-
-    if view_type == "Child Table Diff" and child_table_field:
-        return get_child_table_diff(doctype, docname_filter, child_table_field)
-    else:
-        return get_document_level_diff(doctype, docname_filter)
-
-def get_document_level_diff(doctype, docname_filter):
+    include_empty_fields = filters.get("include_empty_fields")
     columns, data = [], []
 
     meta = frappe.get_meta(doctype)
@@ -26,15 +17,12 @@ def get_document_level_diff(doctype, docname_filter):
         if field.fieldtype not in ["Tab Break", "Section Break", "Column Break"]
     ]
 
-    columns.append({"label": "Document", "fieldname": "docname", "fieldtype": "Data", "width": 150})
-    columns.append({"label": "Change Instance", "fieldname": "change_instance", "fieldtype": "Link", "options": "Version", "width": 150})
-    for field in fields:
-        columns.append({"label": field, "fieldname": field, "fieldtype": "Data", "width": 150})
-    columns.append({"label": "Changed By", "fieldname": "changed_by", "fieldtype": "Data", "width": 150})
-    columns.append({"label": "Changed On", "fieldname": "timestamp", "fieldtype": "Datetime", "width": 150})
-
     docname_filters = {"name": ["in", docname_filter]} if docname_filter else {}
     documents = frappe.get_all(doctype, fields=["name"], filters=docname_filters)
+
+    field_value_tracker = {field: [] for field in fields}
+
+    temp_data = []
 
     for doc in documents:
         docname = doc.name
@@ -56,17 +44,21 @@ def get_document_level_diff(doctype, docname_filter):
                 if field_name in initial_values and initial_values[field_name] == current_values[field_name]:
                     initial_values[field_name] = old_value
 
-        data.append({
+        initial_row = {
             "docname": docname,
             "change_instance": "Initial Value",
             **initial_values,
             "changed_by": current_doc.owner,
             "timestamp": get_datetime(current_doc.creation)
-        })
+        }
+        temp_data.append(initial_row)
+
+        for field in fields:
+            field_value_tracker[field].append(initial_row.get(field))
 
         for log in version_logs:
             version_data = json.loads(log["data"])
-            if not version_data.get("changed"):
+            if not any(key in version_data for key in ["added", "changed", "data_import", "removed", "row_changed"]):
                 continue
 
             row = {
@@ -75,68 +67,54 @@ def get_document_level_diff(doctype, docname_filter):
                 "changed_by": log["modified_by"],
                 "timestamp": get_datetime(log["modified"])
             }
+
             for field in fields:
-                row[field] = ""
+                row[field] = "No Change"
+
             for change in version_data.get("changed", []):
                 field_name, _, new_value = change
                 if field_name in row:
                     row[field_name] = new_value
-            data.append(row)
 
-        data.append({
+            for field in fields:
+                field_value_tracker[field].append(row.get(field))
+
+            temp_data.append(row)
+
+        current_row = {
             "docname": docname,
             "change_instance": "Current Value",
             **current_values,
             "changed_by": "-",
             "timestamp": "-"
-        })
+        }
+        temp_data.append(current_row)
 
-    return columns, data
+        for field in fields:
+            field_value_tracker[field].append(current_row.get(field))
 
-def get_child_table_diff(doctype, docname_filter, child_table_field):
+    # Determine which fields to keep based on nulls
+    if not include_empty_fields:
+        fields = [field for field in fields if any(val not in [None, ""] for val in field_value_tracker[field])]
+
+    # Define final columns
     columns = [
         {"label": "Document", "fieldname": "docname", "fieldtype": "Data", "width": 150},
-        {"label": "Change Instance", "fieldname": "change_instance", "fieldtype": "Link", "options": "Version", "width": 150},
-        {"label": "Row ID", "fieldname": "row_id", "fieldtype": "Data", "width": 100},
-        {"label": "Field", "fieldname": "field", "fieldtype": "Data", "width": 150},
-        {"label": "Old Value", "fieldname": "old_value", "fieldtype": "Data", "width": 200},
-        {"label": "New Value", "fieldname": "new_value", "fieldtype": "Data", "width": 200},
-        {"label": "Changed By", "fieldname": "changed_by", "fieldtype": "Data", "width": 120},
-        {"label": "Changed On", "fieldname": "timestamp", "fieldtype": "Datetime", "width": 150}
+        {"label": "Change Instance", "fieldname": "change_instance", "fieldtype": "Link", "options": "Version", "width": 150}
     ]
 
-    data = []
-    docname_filters = {"name": ["in", docname_filter]} if docname_filter else {}
-    documents = frappe.get_all(doctype, fields=["name"], filters=docname_filters)
+    for field in fields:
+        columns.append({"label": field, "fieldname": field, "fieldtype": "Data", "width": 150})
 
-    for doc in documents:
-        docname = doc.name
+    columns.extend([
+        {"label": "Changed By", "fieldname": "changed_by", "fieldtype": "Data", "width": 150},
+        {"label": "Changed On", "fieldname": "timestamp", "fieldtype": "Datetime", "width": 150}
+    ])
 
-        version_logs = frappe.get_all(
-            "Version",
-            filters={"ref_doctype": doctype, "docname": docname},
-            fields=["name", "data", "modified_by", "modified"],
-            order_by="modified ASC"
-        )
+    # Filter data again to remove fields not selected
+    final_data = []
+    for row in temp_data:
+        filtered_row = {k: v for k, v in row.items() if k in fields or k in ["docname", "change_instance", "changed_by", "timestamp"]}
+        final_data.append(filtered_row)
 
-        for log in version_logs:
-            version_data = json.loads(log["data"])
-            for row_change in version_data.get("row_changed", []):
-                if row_change.get("parentfield") != child_table_field:
-                    continue
-
-                row_id = row_change.get("row")
-                for change in row_change.get("changed", []):
-                    fieldname, old_value, new_value = change
-                    data.append({
-                        "docname": docname,
-                        "change_instance": log["name"],
-                        "row_id": row_id,
-                        "field": fieldname,
-                        "old_value": old_value,
-                        "new_value": new_value,
-                        "changed_by": log["modified_by"],
-                        "timestamp": get_datetime(log["modified"])
-                    })
-
-    return columns, data
+    return columns, final_data
