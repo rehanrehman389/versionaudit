3741e0b2b269f1784399876b8e2094c128616556
Rehan Ansari <32939507+rehanrehman389@users.noreply.github.com>
2025-06-28 17:25:09 +0530

    Update version_audit_2.py

--- versionaudit/versionaudit/report/version_audit_2/version_audit_2.py
+++ versionaudit/versionaudit/report/version_audit_2/version_audit_2.py
@@ -7,45 +7,41 @@ from frappe.utils import get_datetime
 
 def execute(filters=None):
     doctype = filters.get("doctype")
-    docname_filter = filters.get("docname")  # Get the optional document name filter (can be a list)
+    docname_filter = filters.get("docname")
+    view_type = filters.get("view_type") or "Document Level"
+    child_table_field = filters.get("child_table")
     columns, data = [], []
 
-    # Dynamically retrieve all fields for the specified doctype, excluding layout fields
+    if view_type == "Child Table Diff" and child_table_field:
+        return get_child_table_diff(doctype, docname_filter, child_table_field)
+    else:
+        return get_document_level_diff(doctype, docname_filter)
+
+def get_document_level_diff(doctype, docname_filter):
+    columns, data = [], []
+
     meta = frappe.get_meta(doctype)
     fields = [
         field.fieldname for field in meta.fields
         if field.fieldtype not in ["Tab Break", "Section Break", "Column Break"]
     ]
 
-    # Define columns dynamically based on the doctype fields
     columns.append({"label": "Document", "fieldname": "docname", "fieldtype": "Data", "width": 150})
-    columns.append({
-        "label": "Change Instance",
-        "fieldname": "change_instance",
-        "fieldtype": "Link",
-        "options": "Version",  # This will create a linkable field based on the "Version" doctype
-        "width": 150
-    })
+    columns.append({"label": "Change Instance", "fieldname": "change_instance", "fieldtype": "Link", "options": "Version", "width": 150})
     for field in fields:
         columns.append({"label": field, "fieldname": field, "fieldtype": "Data", "width": 150})
     columns.append({"label": "Changed By", "fieldname": "changed_by", "fieldtype": "Data", "width": 150})
     columns.append({"label": "Changed On", "fieldname": "timestamp", "fieldtype": "Datetime", "width": 150})
 
-    # Fetch documents based on filters
     docname_filters = {"name": ["in", docname_filter]} if docname_filter else {}
     documents = frappe.get_all(doctype, fields=["name"], filters=docname_filters)
 
     for doc in documents:
         docname = doc.name
-
-        # Fetch the document's current values
         current_doc = frappe.get_doc(doctype, docname)
         current_values = {field: current_doc.get(field) for field in fields}
-
-        # Initialize initial values from the current document
         initial_values = {field: current_values[field] for field in fields}
 
-        # Fetch all version logs for this document, ordered by modification date (ascending)
         version_logs = frappe.get_all(
             "Version",
             filters={"ref_doctype": doctype, "docname": docname},
@@ -53,68 +49,94 @@ def execute(filters=None):
             order_by="modified ASC"
         )
 
-        # Update initial values dynamically based on the version logs
         for log in version_logs:
             version_data = json.loads(log["data"])
-
-            # Skip logs that do not have any of the specified keys
-            if not any(key in version_data for key in ["added", "changed", "data_import", "removed", "row_changed"]):
-                continue
-
             for change in version_data.get("changed", []):
                 field_name, old_value, _ = change
-
-                # Set the initial value from the log only if it's not already captured
                 if field_name in initial_values and initial_values[field_name] == current_values[field_name]:
                     initial_values[field_name] = old_value
 
-        # Add the initial row with captured initial values
-        initial_row = {
+        data.append({
             "docname": docname,
             "change_instance": "Initial Value",
-            **initial_values,  # Include the initial values here
+            **initial_values,
             "changed_by": current_doc.owner,
             "timestamp": get_datetime(current_doc.creation)
-        }
-        data.append(initial_row)
+        })
 
-        # Process version logs and capture changes for each change instance
-        for idx, log in enumerate(version_logs):
+        for log in version_logs:
             version_data = json.loads(log["data"])
-
-            # Skip logs that do not have any of the specified keys
-            if not any(key in version_data for key in ["added", "changed", "data_import", "removed", "row_changed"]):
+            if not version_data.get("changed"):
                 continue
 
-            # Prepare a row for each version change
             row = {
                 "docname": docname,
                 "change_instance": log["name"],
                 "changed_by": log["modified_by"],
                 "timestamp": get_datetime(log["modified"])
             }
-
-            # Mark all fields as blank initially
             for field in fields:
-                row[field] = ""  # Set to blank for all fields initially
-
-            # Update the row with changes, but only for fields that have changed
+                row[field] = ""
             for change in version_data.get("changed", []):
                 field_name, _, new_value = change
                 if field_name in row:
-                    row[field_name] = new_value  # Update the row with new values
-
-            # Add the row for this change to the data
+                    row[field_name] = new_value
             data.append(row)
 
-        # Add the last row with current values
-        current_row = {
+        data.append({
             "docname": docname,
             "change_instance": "Current Value",
-            **current_values,  # Include the current values here
-            "changed_by": "-",  # No specific user for current values
-            "timestamp": "-"    # No specific timestamp for current values
-        }
-        data.append(current_row)
+            **current_values,
+            "changed_by": "-",
+            "timestamp": "-"
+        })
+
+    return columns, data
+
+def get_child_table_diff(doctype, docname_filter, child_table_field):
+    columns = [
+        {"label": "Document", "fieldname": "docname", "fieldtype": "Data", "width": 150},
+        {"label": "Change Instance", "fieldname": "change_instance", "fieldtype": "Link", "options": "Version", "width": 150},
+        {"label": "Row ID", "fieldname": "row_id", "fieldtype": "Data", "width": 100},
+        {"label": "Field", "fieldname": "field", "fieldtype": "Data", "width": 150},
+        {"label": "Old Value", "fieldname": "old_value", "fieldtype": "Data", "width": 200},
+        {"label": "New Value", "fieldname": "new_value", "fieldtype": "Data", "width": 200},
+        {"label": "Changed By", "fieldname": "changed_by", "fieldtype": "Data", "width": 120},
+        {"label": "Changed On", "fieldname": "timestamp", "fieldtype": "Datetime", "width": 150}
+    ]
+
+    data = []
+    docname_filters = {"name": ["in", docname_filter]} if docname_filter else {}
+    documents = frappe.get_all(doctype, fields=["name"], filters=docname_filters)
+
+    for doc in documents:
+        docname = doc.name
+
+        version_logs = frappe.get_all(
+            "Version",
+            filters={"ref_doctype": doctype, "docname": docname},
+            fields=["name", "data", "modified_by", "modified"],
+            order_by="modified ASC"
+        )
+
+        for log in version_logs:
+            version_data = json.loads(log["data"])
+            for row_change in version_data.get("row_changed", []):
+                if row_change.get("parentfield") != child_table_field:
+                    continue
+
+                row_id = row_change.get("row")
+                for change in row_change.get("changed", []):
+                    fieldname, old_value, new_value = change
+                    data.append({
+                        "docname": docname,
+                        "change_instance": log["name"],
+                        "row_id": row_id,
+                        "field": fieldname,
+                        "old_value": old_value,
+                        "new_value": new_value,
+                        "changed_by": log["modified_by"],
+                        "timestamp": get_datetime(log["modified"])
+                    })
 
     return columns, data
